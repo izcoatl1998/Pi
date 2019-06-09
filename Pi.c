e2f79d464b729c654932cf340a6b239c9ca63e2e
chris rivas <irivas@ucol.mx>
2019-06-09 14:22:56 -0500

    habilitamos el adc an0 y an1

--- Pi.c
+++ Pi.c
@@ -1,13 +1,15 @@
 #include <Pi.h>
-
 void main()
 {
+   setup_adc(ADC_CLOCK_INTERNAL);
+   setup_adc_ports(AN0_TO_AN1);
+   setup_adc_ports(AN0);
+   setup_adc(ADC_CLOCK_INTERNAL);
+   while(TRUE)
+   {
 
-	while(TRUE)
-	{
 
-
-		//TODO: User Code
-	}
+      
+   }
 
 }
