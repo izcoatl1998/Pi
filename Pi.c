89012bd07321fc4d0679bfa4c5e8dc66d4b47fb9
chris rivas <irivas@ucol.mx>
2019-06-09 16:46:00 -0500

    habilitar puerto serial

--- Pi.c
+++ Pi.c
@@ -1,6 +1,7 @@
 #include <Pi.h>
 int lectura_adc=0 ;
 int16 valor_lectura=0;
+int flag_an0=0, flag_an1=0;
 #INT_AD
 void isr_adc(){
    valor_lectura = read_adc(adc_read_only);
@@ -14,9 +15,10 @@ void main()
    setup_adc_ports(AN0_TO_AN1);
    setup_adc_ports(AN0);
    setup_adc(ADC_CLOCK_INTERNAL);
+   
    while(TRUE)
    {
-
+      
 
       
    }
--- Pi.h
+++ Pi.h
@@ -1,7 +1,19 @@
 #include <18F4620.h>
 #device ADC=10
+#include <stdio.h>
+#include <stdlib.h> 
 
-#FUSES NOWDT                 	//No Watch Dog Timer
+#FUSES NOWDT                    //No Watch Dog Timer
+#FUSES NOFCMEN                  //Fail-safe clock monitor disabled
+#FUSES NOIESO                   //Internal External Switch Over mode disabled
+#FUSES PUT                      //Power Up Timer
+#FUSES NOBROWNOUT               //No brownout reset
+#FUSES NOPBADEN                 //PORTB pins are configured as digital I/O on RESET
+#FUSES NOLPT1OSC                //Timer1 configured for higher power operation
+#FUSES NOLVP                    //No low voltage prgming, B3(PIC16) or B5(PIC18) used for I/O
+#FUSES NOXINST                  //Extended set extension and Indexed Addressing mode disabled (Legacy mode)
 
-#use delay(crystal=16MHz)
+#use delay(crystal=16000000)
+#use rs232(baud=9600,parity=N,xmit=PIN_C6,rcv=PIN_C7,bits=8,stream=PORT1)
+
 
