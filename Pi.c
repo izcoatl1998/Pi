91e7a911f4ccd4d0c2e78aa2662a3163a3ff772f
chris rivas <irivas@ucol.mx>
2019-06-09 19:17:42 -0500

    agrego otra variable para el potenciometro/lm35 para la temperatura)

--- Pi.c
+++ Pi.c
@@ -1,7 +1,7 @@
 #include <Pi.h>
 int lectura_adc=0 ;
-int16 valor_lectura=0,pot_humedad=0,pot_presion=0;
-int flag_an0=0, flag_an1=0;
+int16 valor_lectura=0,pot_humedad=0,pot_presion=0,pot_temperatura;
+int flag_an0=0, flag_an1=0,flag_an2=0;
 
 #INT_AD
 void isr_adc(){
@@ -10,19 +10,18 @@ void isr_adc(){
 }
 void main()
 {
+   setup_adc(ADC_CLOCK_INTERNAL | ADC_TAD_MUL_4);
    enable_interrupts(INT_AD);
    enable_interrupts(GLOBAL);
-   setup_adc(ADC_CLOCK_INTERNAL);
-   setup_adc_ports(AN0_TO_AN1);
-   setup_adc_ports(AN0);
-   setup_adc(ADC_CLOCK_INTERNAL);
+   setup_adc_ports(AN0_TO_AN2);
    flag_an0=1;
    set_adc_channel(0);
    read_adc(ADC_START_ONLY);
    while(TRUE)
    {
          if(flag_an0 == 1 && lectura_adc == 1){
-         pot_humedad = valor_lectura;
+         pot_temperatura = valor_lectura;
+         pot_temperatura =(pot_humedad*150)/308;
          flag_an0 = 0;
          flag_an1 = 1;
          lectura_adc = 0;
@@ -31,15 +30,25 @@ void main()
       }
          if(flag_an1 == 1 && lectura_adc == 1){
          pot_presion = valor_lectura;
+         flag_an0 = 0;
+         flag_an1 = 0;
+         flag_an2=1;
+         lectura_adc = 0;
+         set_adc_channel(2);
+         read_adc(ADC_START_ONLY);
+      }
+      if(flag_an2 == 1 && lectura_adc == 1){
+         pot_temperatura = valor_lectura;
          flag_an0 = 1;
          flag_an1 = 0;
+         flag_an2=0;
          lectura_adc = 0;
          set_adc_channel(0);
          read_adc(ADC_START_ONLY);
       }
       printf("\f");
-      printf("valor %li: ",pot_humedad);
-      
+      printf("valor: %li ",pot_humedad);
+      printf("\r valor: %li ",pot_presion);
       
       
    }
